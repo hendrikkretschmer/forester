763c9c3d6e921e046368faa4b12a403cecb47f25
Hendrik Kretschmer <hendrikkretschmer@Hendriks-MBP.local>
2020-11-30 17:02:30 +0100

    Forester Map neu

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,17 +3,6 @@ import React, { Component } from 'react';
 import L from 'leaflet';
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'; 
 
-var greenIcon = L.icon({
-    iconUrl: 'leaf-green.png',
-    shadowUrl: 'leaf-shadow.png',
-
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
-
 class Map extends Component {
   state = {
     lat: 53.551086,
@@ -21,14 +10,33 @@ class Map extends Component {
     zoom: 13,
   }
 
+
+  //Standortabfrage//
+  componentDidMount() {
+      navigator.geolocation.getCurrentPosition((position)=> {
+          console.log(position);
+      });
+  }
+  //Standortabfrage//
+
+  //Marker//
+
+  //Marker//
+
+
   render() {
     const position = [this.state.lat, this.state.lng];
 
 return (
   <MapContainer className="map" center={position} zoom={this.state.zoom} scrollWheelZoom={false}>
     <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      //attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'//
+      //url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"//
+      //url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'//
+      //url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"//
+      //url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"//
+      url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+      
     />
     <Marker position={position}>
       <Popup>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Profile.css';
 import homeicon from '../icons/home_icon_25x25.svg';
+import profilepicture from '../images/forest_fog3.jpg';
 
 
 function Profile() {
@@ -8,10 +9,33 @@ function Profile() {
     return (
         <div>
             <div className="container__profile--picture">
-            <img src={homeicon} width="50" height="50" alt="Homeicon" />
-            </div>
+                <img className="profile__picture--wrapper" src={profilepicture} width="200" height="200" alt="ProfilePicture" />
+
+            
             <div className="container__username--headline2">
-                <h2>Username</h2>
+                <h2>Horst Forst</h2>
+            </div>
+            </div>
+            
+            <div className="container__profile--data">
+                <div className="profile__data--item">
+                <img src={homeicon} width="50" height="50" alt="Homeicon" />
+                <h3 className="profile__data--headline3">Forests visited</h3>
+                </div>
+
+                <div className="profile__data--item">
+                <img src={homeicon} width="50" height="50" alt="Homeicon" />
+                <h3 className="profile__data--headline3">Found Treasures</h3>
+                </div>
+
+                <div className="profile__data--item">
+                <img src={homeicon} width="50" height="50" alt="Homeicon" />
+                <h3 className="profile__data--headline3">Data</h3>
+                </div>
+
+                <div className="profile__data--item"><img src={homeicon} width="50" height="50" alt="Homeicon" />
+                <h3 className="profile__data--headline3">Data</h3>
+                </div>
             </div>
         </div>
     )
