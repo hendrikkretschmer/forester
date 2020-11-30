4621d50080211dfac22605ba9f11a79eb33dab91
Hendrik Kretschmer <hendrikkretschmer@Hendriks-MBP.local>
2020-11-30 18:31:48 +0100

    Forester letzter Stand

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,17 @@
 import './Map.css';
 import React, { Component } from 'react';
-import L from 'leaflet';
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'; 
+
+
+{/*import L from 'leaflet';
+  function GetIcon(_iconSize){
+  return L.icon(options {
+    iconUrl: require('../icons/location icons/treeicon1.png'),
+    iconSize: [20]
+
+  }
+  );
+*/}
 
 class Map extends Component {
   state = {
@@ -30,22 +40,33 @@ class Map extends Component {
 return (
   <MapContainer className="map" center={position} zoom={this.state.zoom} scrollWheelZoom={false}>
     <TileLayer
-      //attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'//
-      //url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"//
-      //url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'//
-      //url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"//
-      //url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"//
       url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-      
     />
+
     <Marker position={position}>
+    <Popup></Popup>
+
+
+    </Marker>
+    
+    {/*<Marker position={position}>
       <Popup>
         Fischbeker Heide <br /> Plan your woodventure!.
       </Popup>
-    </Marker>
+    </Marker>*/}
+
   </MapContainer>
   )
   }
 }
 
 export default Map;
+
+
+
+
+//attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'//
+      //url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"//
+      //url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'//
+      //url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"//
+      //url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"//
