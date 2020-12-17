bc0a12def61205599cef615fb48da5c3c962dcd2
Hendrik Kretschmer <hendrikkretschmer@Hendriks-MBP.local>
2020-12-17 10:36:39 +0100

    homestart

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './SearchBar.css';
+import explore_icon_25x25 from '../icons/explore_icon_25x25.svg';
 
 function SearchBar() {
 
     return (
+        <div className ="searchbar__section--wrapper">
+            <div className="searchbar__top--wrapper">
 
-        <div className="slider__component--wrapper">
+            </div>
+            <div className="searchbar__section--top"></div>
+
+
+            <div className="input__component--wrapper">
+                <div className="input__wrapper">
+                <div className="input__label--wrapper">
+                <img src={explore_icon_25x25} className="input__icon" alt="input__icon" />
+                
+                <label className="label__title">Your current location</label>
+                </div>
+
+                <input type="text" placeholder="type in your current position" className="input__field"></input>
+                </div>
+                
+
+            </div>
             
-         </div>
+        </div>
     )
 }
 
